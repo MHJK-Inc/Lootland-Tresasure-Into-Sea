d8b15110715041af69e69ffcc4d062815231c4e2
Kristian Lazo <56580883+Lazokris@users.noreply.github.com>
2023-04-21 16:41:22 -1000

    update

---
+++ Lootlantis/Assets/Code/Scripts/BoulderAttack.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoulderAttack : MonoBehaviour
+{
+    GameObject target;
+    public float speed;
+    public float life;
+    Rigidbody2D bulletRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        life = 200;
+        bulletRB = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player");
+        Vector2 moveDir = (target.transform.position - transform.position).normalized * speed;
+        bulletRB.velocity = new Vector2(moveDir.x, moveDir.y);
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Time.timeScale != 0f)
+        {
+            //If life of bullet is 0 or less, remove gameObject
+            if (life > 0)
+            {
+                life--;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            collider.gameObject.GetComponent<Player>().TakeDamage(5);
+            Destroy(gameObject);
+        }
+    }
+}
